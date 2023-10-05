c1c4fc4a40f78cda7328f209a212682e47323951
VLN37 <jfpr1237@gmail.com>
2023-10-05 12:42:47 -0300

    readability headers :3

---
+++ syscalls_x64.asm
@@ -0,0 +1,16 @@
+%ifndef __SYSCALLS_X64__
+%define __SYSCALLS_X64__ 0
+
+%define __READ 0
+%define __WRITE 1
+%define __OPEN 2
+%define __CLOSE 3
+%define __STAT 4
+%define __LSEEK 8
+%define __ACCESS 21
+%define __DUP 32
+%define __DUP2 33
+%define __EXECVE 59
+%define __EXIT 60
+
+%endif
