8013a6494d9ffe90365303c3929aafc199ebf254
VLN37 <jfpr1237@gmail.com>
2023-10-07 16:56:15 -0300

    improve testing

--- tests/main.c
+++ tests/main.c
@@ -3,23 +3,28 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define true 0
-#define false 1
-
 extern void hello_world();
 extern int ft_strlen();
 extern char *ft_strcpy();
+void test_strlen();
+void test_strcpy();
 
 int main(void) {
-    char string[] = "salada";
-
-    string[2] = '\0';
     hello_world();
+    test_strlen();
+    test_strcpy();
+}
+
+void test_strlen(void) {
+    char string[] = "salada";
+    string[2] = '\0';
 
     assert(ft_strlen("ararar") == 6);
     assert(ft_strlen("") == 0);
     assert(ft_strlen(string) == 2);
+}
 
+void test_strcpy(void) {
     // copies the immutable strings to the char arrays on the stack
     char src1[100] = "abc";
     char dst1[100] = "cdef";
@@ -44,4 +49,18 @@ int main(void) {
     strcpy(dst2, src2);
     assert(strcmp(dst2, dst1) == 0);
     assert(*src1 == '\0');
+
+    strcpy(src1, "");
+    strcpy(dst1, "");
+    strcpy(src2, "");
+    strcpy(dst2, "");
+
+    ft_strcpy(dst1, src1);
+    strcpy(dst2, src2);
+    assert(strcmp(dst2, dst1) == 0);
+    assert(*src1 == '\0');
+
+    printf("%d\n", strcmp("asd", "asdf"));
+    printf("%d\n", strcmp("asdf", "asd"));
+    printf("%d\n", strcmp("asdf", "asdf"));
 }
