6256a84b7080702596e0b458566c4d7847b50b68
VLN37 <jfpr1237@gmail.com>
2023-10-09 10:32:54 -0300

    enhance: add_front error handling

--- src/ft_list_add_front.asm
+++ src/ft_list_add_front.asm
@@ -4,6 +4,11 @@ section .text
     global ft_list_add_front
 
 ft_list_add_front:
+    test rdi, rdi
+    jz exit
     mov rdx, [rdi]
     mov [rsi + 8], rdx  ; new->next = *begin
     mov [rdi], rsi      ; *begin = new
+
+exit:
+    ret
--- tests/main_bonus.c
+++ tests/main_bonus.c
@@ -5,12 +5,13 @@ int main(void) {
     printf("Hello, Bonus\n");
     test_list_size();
     test_push_front();
+    printf("Goodbye, Bonus\n");
 }
 
 void test_list_size() {
-    t_list obj1;
-    t_list obj2;
-    t_list *head;
+    t_list  obj1;
+    t_list  obj2;
+    t_list  *head;
 
     head = &obj1;
     obj1.data = (void *)0x42;
@@ -23,14 +24,17 @@ void test_list_size() {
 }
 
 void test_push_front() {
-    t_list obj1;
-    t_list obj2;
-    t_list *head;
+    t_list  obj1;
+    t_list  obj2;
+    t_list  *head;
 
     obj1.data = (void *)0x42;
     obj1.next = NULL;
     obj2.data = (void *)0x43;
     obj2.next = NULL;
+    head = NULL;
+    // test error handling
+    ft_list_add_front(NULL, &obj2);
     head = &obj1;
     assert(head->data == (void *)0x42);
     ft_list_add_front(&head, &obj2);
